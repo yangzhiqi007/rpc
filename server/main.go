af5f4c1bdcd4b76d002e1869526b66f8f8635275
yangzhiqi <yangzhiqi@yangzhiqideMac-mini.local>
2018-05-21 15:49:17 +0800

    save

--- server/main.go
+++ server/main.go
@@ -6,12 +6,18 @@ import (
 	"rpc/server/logic"
 	pb "rpc/struct"
 
+	"database/sql"
+
+	mysql "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
 )
 
 const Port = ":50051"
 
 func main() {
+
+	db, _ := sql.Open("mysql", "")
+
 	lis, err := net.Listen("tcp", Port)
 	if err != nil {
 		log.Fatal("failed to listen: %v", err)
